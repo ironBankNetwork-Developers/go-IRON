840bf0ba43a6170b9e258a46d603739882b96188
Alber Erre <arblugo@gmail.com>
2017-12-26 02:41:01 +0000

    iron path and allocs

--- core/genesis.go
+++ core/genesis.go
@@ -314,7 +314,7 @@ func ironGenesisBlock() *Genesis {
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
 		GasLimit:   7654321,
 		Difficulty: big.NewInt(4000),
-		Alloc:      decodePrealloc(ironAllocData),
+		Alloc:      nil,
 	}
 }
 
