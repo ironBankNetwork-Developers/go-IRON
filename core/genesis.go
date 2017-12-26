7e5ae3a3a47a7078335cbb0c8deac8f7f3bba636
Alber Erre <arblugo@gmail.com>
2017-12-26 01:33:32 +0000

    g

--- core/genesis.go
+++ core/genesis.go
@@ -311,7 +311,7 @@ func ironGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
 		Nonce:      66,
-		ExtraData:  hexutil.MustDecode("0x000000000000000000000000000000000000000000000000000002601202017"),
+		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
 		GasLimit:   7654321,
 		Difficulty: big.NewInt(4000),
 		Alloc:      decodePrealloc(mainnetAllocData),
