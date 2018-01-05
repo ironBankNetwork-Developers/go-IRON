aa540f234c00bb29dc12f6d8a806536f81eae96e
iron Bank Network <33318933+iron-bank@users.noreply.github.com>
2018-01-05 16:46:56 +0100

    genesis

--- core/genesis.go
+++ core/genesis.go
@@ -312,8 +312,8 @@ func ironGenesisBlock() *Genesis {
 		Config:     params.MainnetChainConfig,
 		Nonce:      66,
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   7654321,
-		Difficulty: big.NewInt(4000),
+		GasLimit:   5000,
+		Difficulty: big.NewInt(1),
 		Alloc:      nil,
 	}
 }
