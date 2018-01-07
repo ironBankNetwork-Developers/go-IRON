ef3e81a5ac9db597c360179bfb30810154e0fe2a
iron Bank Network <33318933+iron-bank@users.noreply.github.com>
2018-01-07 22:12:59 +0100

    Update genesis.go

--- core/genesis.go
+++ core/genesis.go
@@ -158,8 +158,8 @@ func SetupGenesisBlock(db ethdb.Database, genesis *Genesis) (*params.ChainConfig
 	stored := GetCanonicalHash(db, 0)
 	if (stored == common.Hash{}) {
 		if genesis == nil {
-			log.Info("Writing default main-net genesis block")
-			genesis = DefaultGenesisBlock()
+			log.Info("Writing default iron genesis block")
+			genesis = ironGenesisBlock()
 		} else {
 			log.Info("Writing custom genesis block")
 		}
@@ -306,15 +306,15 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 	return g.MustCommit(db)
 }
 
-// DefaultGenesisBlock returns the Ethereum main net genesis block.
-func DefaultGenesisBlock() *Genesis {
+// ironGenesisBlock returns the iron main net genesis block.
+func ironGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
 		Nonce:      66,
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
 		GasLimit:   5000,
 		Difficulty: big.NewInt(1),
-		Alloc:      decodePrealloc(mainnetAllocData),
+		Alloc:      nil,
 	}
 }
 
